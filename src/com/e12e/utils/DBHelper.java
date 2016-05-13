4307a9c5670446aee35ceb0c43115142dcd27e2c
Coande <e12e@qq.com>
2016-05-13 17:09:23 +0800

    use database

--- src/com/e12e/utils/DBHelper.java
+++ src/com/e12e/utils/DBHelper.java
@@ -8,18 +8,26 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import org.junit.Test;
+
 
 public class DBHelper {
+	
+	@Test
+	public void testGetConnection() throws ClassNotFoundException, FileNotFoundException, IOException, SQLException{
+		getConnection();
+	}
+	
 	private static Connection conn;
 	public synchronized static Connection getConnection() throws ClassNotFoundException, FileNotFoundException, IOException, SQLException{
 		if(conn==null){
 			//获取配置
 			Properties properties=new Properties();
-			properties.load(new FileInputStream("WEB-INF/config/db.properties"));
+			properties.load(new FileInputStream("db.properties"));
 			String url=properties.getProperty("url");
-			String user=properties.getProperty("user");
+			String user=properties.getProperty("username");
 			String password=properties.getProperty("password");
-			
+			System.out.println("DB connect success! username="+user+" password="+password);
 			
 			Class.forName("com.mysql.jdbc.Driver");
 			conn=DriverManager.getConnection(url, user, password);
